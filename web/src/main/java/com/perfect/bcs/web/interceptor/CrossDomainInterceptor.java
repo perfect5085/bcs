d57b429de66b34380f9c63c5785771ddccf8f542
梁波 <perfect5085@163.com>
2025-01-26 23:24:33 +0800

    更新

--- web/src/main/java/com/perfect/bcs/web/interceptor/CrossDomainInterceptor.java
+++ web/src/main/java/com/perfect/bcs/web/interceptor/CrossDomainInterceptor.java
@@ -2,8 +2,6 @@ package com.perfect.bcs.web.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -14,8 +12,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  */
 @Component
 public class CrossDomainInterceptor extends HandlerInterceptorAdapter {
-
-    private static final Logger logger = LoggerFactory.getLogger(CrossDomainInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
